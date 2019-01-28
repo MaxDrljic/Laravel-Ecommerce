9dc973d88cc623082acfce4bb80020e8e4433662
MaxDrljic <max_spree@hotmail.com>
2019-01-28 20:06:42 +0100

    Products factory

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,21 +12,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        $p1 = [
-            'name'          => 'Learn to build websites in HTML5',
-            'image'         => 'uploads/products/book.jpg',
-            'price'         => 5000,
-            'description'   => 'Lorem ipsum dolor sit amet, mei ea dolor reprehendunt. Veri dicam an sea, eam ex errem suscipiantur. Dolore argumentum usu no, adhuc equidem iracundia et usu.'
-        ];
-
-        $p2 = [
-            'name'          => 'PHP development in depth',
-            'image'         => 'uploads/products/book.jpg',
-            'price'         => 2400,
-            'description'   => 'Lorem ipsum dolor sit amet, mei ea dolor reprehendunt. Veri dicam an sea, eam ex errem suscipiantur. Dolore argumentum usu no, adhuc equidem iracundia et usu.'
-        ];
-
-        Product::create($p1);
-        Product::create($p2);
+        factory(\App\Product::class, 30)->create();
     }
 }
